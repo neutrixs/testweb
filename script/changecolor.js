8f3339ee2c5446c6be304fffedd6917c3feb0d7e
neutrixs <neutrixs@users.noreply.github.com>
2020-12-15 18:30:28 +0700

    shadow on hover

--- script/changecolor.js
+++ script/changecolor.js
@@ -1,3 +1,11 @@
 function changecolor(docid, changeto){
     document.getElementById(docid).style.backgroundColor = changeto;
 }
+function shadow(docid, want){
+    if(want == 'yes'){   
+        document.getElementById(docid).classList.remove('noshadow')
+    }
+    else{
+        document.getElementById(docid).classList.add('noshadow')
+    }
+}
